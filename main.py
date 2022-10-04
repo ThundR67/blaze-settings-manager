441684f61bf14d7d28aa069810097756a2c2fa10
Roshan J Mehta <sonicroshan122@gmail.com>
2022-10-03 23:54:23 -0400

    add dynamic navigation pane

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.pages = [["Default Applications", "org.gnome.Settings-default-apps-symbolic"]]
 
         self.set_title(title="Settings")
         self.set_default_size(width=int(1366 / 2), height=int(768 / 2))
@@ -31,22 +32,24 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def get_navigation_pane(self):
         list_box = Gtk.ListBox.new()
-        item = Gtk.ListBoxRow.new()
 
-        grid = Gtk.Grid.new()
-        grid.set_column_spacing(12)
-        grid.set_margin_top(6)
-        grid.set_margin_bottom(6)
-        grid.set_margin_start(12)
-        grid.set_margin_end(12)
+        for name, icon in self.pages: 
+            item = Gtk.ListBoxRow.new()
 
-        label = Gtk.Label.new()
-        label.set_text("Default Applications")
+            grid = Gtk.Grid.new()
+            grid.set_column_spacing(12)
+            grid.set_margin_top(6)
+            grid.set_margin_bottom(6)
+            grid.set_margin_start(12)
+            grid.set_margin_end(12)
 
-        icon = Gtk.Image.new_from_icon_name("org.gnome.Settings-default-apps-symbolic")
+            label = Gtk.Label.new()
+            label.set_text(name)
 
-        grid.attach(icon, 0, 0, 1, 1)
-        grid.attach(label, 1, 0, 1, 1)
+            icon = Gtk.Image.new_from_icon_name(icon)
+
+            grid.attach(icon, 0, 0, 1, 1)
+            grid.attach(label, 1, 0, 1, 1)
         
 
         item.set_child(grid)
