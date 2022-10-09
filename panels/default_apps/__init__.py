db25768a465fd0691ec4ed5e9be108c0d4f33523
Roshan Mehta <sonicroshan122@gmail.com>
2022-10-09 15:29:23 -0400

    complete default app panel

--- panels/default_apps/__init__.py
+++ panels/default_apps/__init__.py
@@ -1,4 +1,5 @@
 """Panel to customize default applications."""
+from dataclasses import dataclass
 import gi
 
 gi.require_version(namespace='Gtk', version='4.0')
@@ -8,12 +9,12 @@ from gi.repository import Adw, Gtk
 
 
 LABELS = {
-    "Web": "x-scheme-handler/http",
-    "Mail": "x-scheme-handler/mailto",
-    "Calendar": "text/calendar",
-    "Music": "audio/x-vorbis+ogg",
-    "Videos": "video/x-ogm+ogg",
-    "Photos": "image/jpeg",
+    "x-scheme-handler/http": ["Web", ["text/html", "application/xhtml+xml", "x-scheme-handler/https"]],
+    "x-scheme-handler/mailto": ["Mail", []],
+    "text/calendar": ["Calendar", []],
+    "audio/x-vorbis+ogg": ["Music", ["audio/"]],
+    "video/x-ogm+ogg": ["Videos", ["video/"]],
+    "image/jpeg": ["Photos", ["image/"]],
 }
 
 class DefaultApplications:
@@ -43,11 +44,14 @@ class DefaultApplications:
         grid.set_halign(Gtk.Align.FILL)
 
 
-        for index, (label_name, mime_type) in enumerate(LABELS.items()):
-            label = Gtk.Label.new(label_name)
+        for index, (mime_type, mime_info) in enumerate(LABELS.items()):
+            label = Gtk.Label.new(mime_info[0])
             label.set_css_classes(["dim-label"])
 
             app_chooser = Gtk.AppChooserButton.new(content_type=mime_type)
+            app_chooser.set_show_default_item(True)
+            
+            app_chooser.connect("changed", self.on_app_chooser_changed)
             
             grid.attach(label, 0, index, 1, 1)
             grid.attach_next_to(app_chooser, label, Gtk.PositionType.RIGHT, 1, 1)
@@ -57,3 +61,14 @@ class DefaultApplications:
         adw_bin.set_child(box)
 
         return adw_bin
+
+    def on_app_chooser_changed(self, widget):
+        """Callback for when app chooser is changed."""
+        info = widget.get_app_info()
+        mime_type = widget.get_content_type()
+        info.set_as_default_for_type(mime_type)
+
+        for extra_types in LABELS[mime_type][1]:
+            for supported_type in info.get_supported_types():
+                if supported_type.startswith(extra_types):
+                    info.set_as_default_for_type(supported_type)
